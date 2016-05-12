b031f0d00fe31d22c90f3d3ac840fdbe49f8a0fe
Fabien Fouassier <fabien.fouassier@isen-lille.fr>
2016-05-12 14:09:46 +0200

    Margins corrected, profile activity added

--- app/src/main/java/com/sorbellini/s214631/lab3/Customer.java
+++ app/src/main/java/com/sorbellini/s214631/lab3/Customer.java
@@ -10,23 +10,39 @@ public class Customer implements Parcelable{
     private String name;
     private String surname;
     private String phone;
+    private String photo;
+    private String email;
+    private String birthday;
+    private String password;
 
     //constructor
     public Customer(){
         this.name = null;
         this.surname = null;
+        this.phone = null;
+        this.photo = null;
+        this.email = null;
+        this.password = null;
+        this.birthday = null;
     }
 
     //getter
     public String getName(){ return this.name; }
     public String getSurname(){ return this.surname; }
     public String getPhone(){ return this.phone; }
+    public String getPhoto() { return this.photo; }
+    public String getEmail() { return this.email; }
+    public String getPassword() { return this.password; }
+    public String getBirthday() { return this.birthday; }
 
     //setter
     public void setName(String name){ this.name = name; }
     public void setSurname(String surname){ this.surname = surname; }
     public void setPhone(String phone){ this.phone = phone; }
-
+    public void setPhoto(String photo){ this.photo = photo; }
+    public void setEmail(String email){ this.email = email; }
+    public void setPassword(String password){ this.password = password; }
+    public void setBirthday(String birthday){ this.birthday = birthday; }
 
     @Override
     public int describeContents(){
