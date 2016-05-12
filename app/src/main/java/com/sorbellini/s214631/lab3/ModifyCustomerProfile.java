9064bc1f6e4e2196acc070414d94f915a95ac208
Fabien Fouassier <fabien.fouassier@isen-lille.fr>
2016-05-12 22:10:26 +0200

    Added ModifyCustomerProfile and finished the design

--- app/src/main/java/com/sorbellini/s214631/lab3/ModifyCustomerProfile.java
+++ app/src/main/java/com/sorbellini/s214631/lab3/ModifyCustomerProfile.java
@@ -14,14 +14,12 @@ import com.google.gson.Gson;
 
 import java.util.Locale;
 
-public class CustomerProfile extends AppCompatActivity {
-
-    private Customer profile;
+public class ModifyCustomerProfile extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_customer_profile);
+        setContentView(R.layout.activity_modify_customer_profile);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_offer_details);
         setSupportActionBar(toolbar);
@@ -35,19 +33,27 @@ public class CustomerProfile extends AppCompatActivity {
         if(json != null) {
             profile = gson.fromJson(json, Profile.class);
 
-            ImageView imageView = (ImageView) findViewById(R.id.customer_profile_photo);
+            ImageView imageView = (ImageView) findViewById(R.id.modify_customer_profile_photo);
             imageView.setImageURI(Uri.parse(profile.getPhoto()));
 
-            TextView textView = (TextView) findViewById(R.id.customer_profile_surname);
+            TextView textView = (TextView) findViewById(R.id.modify_customer_profile_surname);
             textView.setText(profile.getSurname());
-            textView = (TextView) findViewById(R.id.customer_profile_name);
+            textView = (TextView) findViewById(R.id.modify_customer_profile_name);
             textView.setText(profile.getName());
-            textView = (TextView) findViewById(R.id.customer_profile_email);
+            textView = (TextView) findViewById(R.id.modify_customer_profile_email);
             textView.setText(profile.getEmail());
-            textView = (TextView) findViewById(R.id.customer_profile_phone);
+            textView = (TextView) findViewById(R.id.modify_customer_profile_phone);
             textView.setText(profile.getPhone());
+            textView = (TextView) findViewById(R.id.modify_customer_profile_password);
+            textView.setText(profile.getPassword());
 
         }
         */
     }
+
+
+    public void saveData() {
+
+    }
+
 }
